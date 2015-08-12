a3675cd75ec5c6d6e620283d33edd5fc61e4319d
satohnorio <satohnorio@gmail.com>
2015-08-12 09:01:18 +0900

    Windowの位置、サイズを保存する機能を追加

---
+++ WpfSavePositionSample/UnitTestProject/UnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfSavePositionSample;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void SettingUnitTest()
+        {
+            MainWindow w = new MainWindow();
+            w.Name = "FMainWindow";
+            w.Left = 50;
+            w.Top = 60;
+            w.Width = 70;
+            w.Height = 80;
+            Setting.Instance.Save(w);
+            var r = Setting.Instance.MainWindowBounds;
+            Assert.IsTrue(50 == r.Left);
+            Assert.IsTrue(60 == r.Top);
+            Assert.IsTrue(70 == r.Width);
+            Assert.IsTrue(80 == r.Height);
+        }
+    }
+}
